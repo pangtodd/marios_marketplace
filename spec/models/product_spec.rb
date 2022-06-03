558d101a1cb89c2a4a398c99932760d729da0f49
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 11:41:36 -0700

    establish test for titleizing names of products.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,9 @@ describe Product do
   it { should validate_presence_of :price }
   it { should validate_numericality_of :price}
   it { should validate_presence_of :country_of_origin}
+
+  it("titleizes the name of a product")do
+  product = Product.create({name:"biggie mushrooms", price: 13.55, country_of_origin: "Italy"})
+  expect(product.name()).to(eq("Biggie Mushrooms"))
+end
 end
