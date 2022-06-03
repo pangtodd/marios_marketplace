0ae5a0034af23ac2f97358407832166e2411e031
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 10:58:46 -0700

    update review.rb to validate 'rating' field only accepts an int of 1-5, passes test.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :rating, numericality: true
+  validates :rating, :numericality => { greater_than_or_equal_to: 1, less_than_or_equal_to: 5  }
 end
