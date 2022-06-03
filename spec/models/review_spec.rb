c984b7bb5a9ba47b5f5b5857b3676eb68466209a
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 09:33:02 -0700

    establish test to validate 'rating' field of reivew form.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,6 @@ describe Review do
   it { should belong_to(:product) }
   it { should validate_presence_of :author}
   it { should validate_presence_of :content_body}
-    # it { should validate_presence_of :country_of_origin}
+  it { should validate_presence_of :rating}
   
 end
