792abd56c073ef8aff8ea25839cf73f720eab126
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 09:54:18 -0700

    update product.rb so 'price'field is validated as a number. passes test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :price, presence: true
+  validates :price, numericality: true
   validates :country_of_origin, presence: true
 end
