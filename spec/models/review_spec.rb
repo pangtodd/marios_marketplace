ec05545a95ed2d74eb67f9ee983b1acdee3b3c0f
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 10:57:11 -0700

    establish test to validate 'rating' field is only filled out with a value between 1-5.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,7 @@ describe Review do
   it { should validate_presence_of :content_body}
   it { should validate_presence_of :rating}
   it { should validate_numericality_of :rating}
+  it {should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
+  it {should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
   
 end
