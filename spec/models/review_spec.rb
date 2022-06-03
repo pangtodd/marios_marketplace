43e88427a9fb1fe42d0bd19affacae5c53597c30
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 09:30:56 -0700

    establish test for reivew form to validate 'content_body' field.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Review do
   it { should belong_to(:product) }
   it { should validate_presence_of :author}
-    # it { should validate_presence_of :price }
+  it { should validate_presence_of :content_body}
     # it { should validate_presence_of :country_of_origin}
   
 end
