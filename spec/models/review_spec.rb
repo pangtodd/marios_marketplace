2a8ff4b4d709a734e7c6f3e4ebc78d632873d9dd
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 09:27:58 -0700

    establish test for review form to validate 'author' field.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product) }
+  it { should validate_presence_of :author}
+    # it { should validate_presence_of :price }
+    # it { should validate_presence_of :country_of_origin}
+  
 end
