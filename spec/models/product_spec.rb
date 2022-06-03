2cb297b23e5c9ff0452c48be184e5b63426291a9
Todd Pangilinan <pang.todd@gmail.com>
2022-06-03 07:33:02 -0700

    establish test to validate 'country of origin' field in product form.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,5 @@ describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
   it { should validate_presence_of :price }
+  it { should validate_presence_of :country_of_origin}
 end
