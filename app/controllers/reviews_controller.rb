2c83d2cb82ec0e243417e0e58835fd1330cd7b37
Todd Pangilinan <pang.todd@gmail.com>
2022-06-02 22:55:40 -0700

    establish initial reviews/show view.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,25 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.new
     render :new
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviewss.new(review_params)
+    if @review.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product =Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  render :show
+end
+
+private
+  def review_params
+    params.require(:review).permit(:rating, :author, :content_body)
+  end
 end
