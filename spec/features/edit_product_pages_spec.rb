2d7ea7d3ff502e8b15394b96539d4bef4c254ac8
Todd Pangilinan <pang.todd@gmail.com>
2022-06-04 10:17:17 -0700

    establish first intergration test for edit product pages. passes test.

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 describe "edit a product process" do
-  it "edits an existing product" do
-    visit new_product_path
-    fill_in 'Name', :with => 'slim jims'
-    fill_in 'Price', :with => 2.69
-    select "United States of America", :from =>"product[country_of_origin]"
+  before(:each)do
+    visit products_path
+    click_link 'Enter new product'
+    fill_in 'Name', :with => 'mango blasters'
+    fill_in 'Price', :with => 10.99
+    select "Italy", :from =>"product[country_of_origin]"
     click_on 'Create Product'
-    click_on('Slim Jims', match: :first)
-    click_on "Add a review"
-    fill_in "Author", :with=> "Skinny Pete"
-    fill_in "Content body", :with=> "I dunno, I'd like them to to be a little less slim. I'd consider buying a Thick Jim."
-    fill_in "Rating", :with=> 3
-    click_on "Create Review"
-    expect(page).to have_content 'Review successfully added!'
-    expect(page).to have_content '3 stars, review by Skinny Pete'
+  end
+  it "edits an existing product" do
+    visit products_path
+    click_on('Mango Blasters', match: :first)
+    click_on('Edit')
+    fill_in 'Name', :with =>'mega mango blasters'
+    fill_in "Price", :with =>12.99
+    select "United States of America", :from =>"product[country_of_origin]"
+    click_on 'Update Product'
+    expect(page).to have_content 'Product successfully updated!'
+    expect(page).to have_content 'Mega Mango Blasters'
   end
 end
