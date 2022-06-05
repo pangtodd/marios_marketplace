378a62c40c8e2230405b750feb4f1b8524d3a7fb
Todd Pangilinan <pang.todd@gmail.com>
2022-06-04 20:04:37 -0700

    update alerts, added to any branching logic.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,10 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
+      flash[:notice] = "Review successfully added!"
       redirect_to product_path(@product)
     else
+      flash[:alert] = "oops, we have an error. Let's try that again."
       render :new
     end
   end
@@ -30,6 +32,7 @@ class ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     if @review.update(review_params)
+      flash[:notice] = "Review successfully updated!"
       redirect_to product_path(@review.product)
     else
       @product = Product.find(params[:product_id])
